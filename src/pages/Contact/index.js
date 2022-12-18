b0cbd55146d8d0328e7824524225e8eac362714b
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-18 08:43:17 -0600

    adding text area above form in contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -20,41 +20,54 @@ function ContactForm(props) {
 
   return (
     <div>
-      <form className="contact-form" onSubmit={handleSubmit}>
-        {/* Name input  */}
-        <div className='form-outline mb-4'>
-            <input
-            type="text"
-            placeholder="Your name"
-            value={input}
-            name="text"
-            className="contact-input"
-            onChange={handleChange}
-            ></input>
+      <h1>Contact Me</h1>
+      <p>
+        Email:
+      </p>
+      <p>
+        GitHub:
+      </p>
+      <p>
+        LinkedIn:
+      </p>
+    
+        <div>
+        <form className="contact-form" onSubmit={handleSubmit}>
+            {/* Name input  */}
+            <div className='form-outline mb-4'>
+                <input
+                type="text"
+                placeholder="Your name"
+                value={input}
+                name="text"
+                className="contact-input"
+                onChange={handleChange}
+                ></input>
+            </div>
+            <div className='form-outline mb-4'>
+                <input
+                type="email"
+                placeholder="Email address"
+                value={input}
+                name="text"
+                className="contact-input"
+                onChange={handleChange}
+                ></input>
+            </div>
+            <div className='form-outline mb-4'>
+                <textarea
+                rows='4'
+                type="text"
+                placeholder="Message"
+                value={input}
+                name="text"
+                className="contact-input"
+                onChange={handleChange}
+                ></textarea>
+            </div>
+            <button className="contact-button">Send</button>
+        </form>
         </div>
-        <div className='form-outline mb-4'>
-            <input
-            type="email"
-            placeholder="Email address"
-            value={input}
-            name="text"
-            className="contact-input"
-            onChange={handleChange}
-            ></input>
-        </div>
-        <div className='form-outline mb-4'>
-            <textarea
-            rows='4'
-            type="text"
-            placeholder="Message"
-            value={input}
-            name="text"
-            className="contact-input"
-            onChange={handleChange}
-            ></textarea>
-        </div>
-        <button className="contact-button">Send</button>
-      </form>
     </div>
   
   );
