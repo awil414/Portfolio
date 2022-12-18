b97b29894d31afe76efbf77bde385fb2cbee5353
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-18 10:44:12 -0600

    importing icons in footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { VscGithub } from "react-icons/vsc";
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { TfiInstagram } from 'react-icons/tfi';
+import {HiOutlineMail} from 'react-icons/hi';
+
+
