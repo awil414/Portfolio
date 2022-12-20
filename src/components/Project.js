c4639b73d938b658b4c92a19b365c449fdad2e7b
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-20 14:59:39 -0600

    trying out some styling changes to Project card

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,46 +5,69 @@ import {
   MDBCardBody,
   MDBCardTitle,
   MDBCardText,
-  // MDBRow,
-  // MDBCol
+
 } from "mdb-react-ui-kit";
 import { Button } from "react-bootstrap";
 
-// export default function Project({title, description}) {
+
+//OLD CODE 
+// export default function Project(props) {
 //   return (
+//     <MDBCard className='project-card'>
+//       <MDBCardImage
+//         // src='https://mdbootstrap.com/img/new/standard/city/041.webp'
+//         src={props.imgPath}
+//         alt='card-img'
+//         position='top'
+//       />
+//       <MDBCardBody>
+        
+//         <MDBCardTitle>{props.title}</MDBCardTitle>
+        
+//         <MDBCardText style={{ textAlign: "justify" }}>
+//           {props.description}
+//         </MDBCardText>
+//         <Button href={props.ghLink} target='_blank' variant='primary'></Button>
+//         <Button
+//           href={props.demoLink}
+//           target='_blank'
+//           variant='primary'
+//         ></Button>
+//       </MDBCardBody>
+//     </MDBCard>
+    
+//   );
+// }
 
-export default function Project(props) {
+
+
+import { MDBRipple } from 'mdb-react-ui-kit';
+
+export default function Project(props)  {
   return (
-    // <MDBRow className='row-cols-1 row-cols-md-2 g-4'>
-    //   <MDBCol>
-    <MDBCard className='project-card'>
-      <MDBCardImage
-        // src='https://mdbootstrap.com/img/new/standard/city/041.webp'
-        src={props.imgPath}
-        alt='card-img'
-        position='top'
-      />
-      <MDBCardBody>
-        {/* <MDBCardTitle>{title}</MDBCardTitle> */}
-        <MDBCardTitle>{props.title}</MDBCardTitle>
-        {/* <MDBCardText>
-              {description}
-            </MDBCardText> */}
-        <MDBCardText style={{ textAlign: "justify" }}>
-          {props.description}
-        </MDBCardText>
-        <Button href={props.ghLink} target='_blank' variant='primary'></Button>
-        <Button
-          href={props.demoLink}
-          target='_blank'
-          variant='primary'
-        ></Button>
-      </MDBCardBody>
-    </MDBCard>
-    //   </MDBCol>
-
-    // </MDBRow>
+    <MDBRipple className='bg-image' rippleTag='div' rippleColor='light'>
+      <MDBCardImage className='w-100' src={props.imgPath} alt='card-img'/>
+      <a href={props.demoLink}>
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <p className='text-white mb-0'>Deployed / Demo </p>
+          </div>
+        </div>
+        <div className='hover-overlay'>
+          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
+        </div>
+      </a>
+      <a href={props.ghLink}>
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <p className='text-white mb-0'>GitHub</p>
+          </div>
+        </div>
+        <div className='hover-overlay'>
+          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}></div>
+        </div>
+      </a>
+     
+    </MDBRipple>
   );
 }
-
-// export default Project;
