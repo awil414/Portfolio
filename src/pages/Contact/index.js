87fa5fe3893d55dcd12a4caa24c565a256361e22
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-12-21 19:40:31 -0600

    addingto contact page links to contact me and styling

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,30 @@
 import React, { useState } from "react";
+import './style.css';
+import {
+  MDBInput,
+  MDBBtn,
+  MDBCard,
+  MDBTextArea
+} from 'mdb-react-ui-kit';
 
-function ContactForm(props) {
-  const [input, setInput] = useState("");
+
+const styles = {
+  backgroundStyle: {
+    background: "#3E5E82",
+  },
+  headingStyle: {
+    fontSize: "30px",
+    textAlign: "center",
+    verticalAlign: "middle",
+    lineHeight: "60px",
+    color: "white",
+    padding: "10px",
+    marginBottom: "0",
+  },
+};
+
+export default function ContactForm(props) {
+  const [input, setInput] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,59 +34,110 @@ function ContactForm(props) {
       text: input,
     });
 
+//   if (input === 'name') {
+//     setInput(value);
+//   } else if (input === 'email') {
+//     setInput(value);
+//   } else {
+//     setInput(value);
+//   }
+// };
+
+
     setInput("");
-  };
+      };
 
   const handleChange = (e) => {
     setInput(e.target.value);
-  };
+  };  
 
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <p>Email:</p>
-      <p>GitHub:</p>
-      <p>LinkedIn:</p>
-
+    <div fluid style={styles.backgroundStyle} className='contact-page'>
+   
+      <h2 style={styles.headingStyle}>Want to Connect?</h2>
+      <p className='info'>Email: <a href="mailto://amber.wilson.code@gmail.com"> amber.wilson.code@gmail.com</a></p>
+      <p className='info'>GitHub: <a href='https://www.linkedin.com/in/amber-wilson-255272217/'>awil414</a></p>
+      <p className='info'><a href='https://www.linkedin.com/in/amber-wilson-255272217/'>LinkedIn</a></p>
+    
       <div>
-        <form className='contact-form' onSubmit={handleSubmit}>
-          {/* Name input  */}
+        <MDBCard alignment='m-auto align-self-center' className='contact-card' onSubmit={handleSubmit}>
           <div className='form-outline mb-4'>
-            <input
+            <MDBInput 
               type='text'
               placeholder='Your name'
               value={input}
               name='text'
               className='contact-input'
               onChange={handleChange}
-            ></input>
+            ></MDBInput>
           </div>
           <div className='form-outline mb-4'>
-            <input
+            <MDBInput 
               type='email'
               placeholder='Email address'
               value={input}
               name='text'
               className='contact-input'
               onChange={handleChange}
-            ></input>
+            ></MDBInput>
           </div>
           <div className='form-outline mb-4'>
-            <textarea
-              rows='4'
-              type='text'
-              placeholder='Message'
-              value={input}
-              name='text'
-              className='contact-input'
-              onChange={handleChange}
-            ></textarea>
+            <MDBTextArea
+                rows='4'
+                type='text'
+                placeholder='Message'
+                value={input}
+                name='text'
+                className='contact-input'
+                onChange={handleChange}>
+            </MDBTextArea>
           </div>
-          <button className='contact-button'>Send</button>
-        </form>
+
+          <MDBBtn type='submit' className='mb-4' block>
+            Send
+          </MDBBtn>
+        </MDBCard> 
       </div>
     </div>
-  );
-}
+  )
+};
 
-export default ContactForm;
+
+
+
+    // {/* {/* <form className='contact-form' onSubmit={handleSubmit}>
+    //   <div className='form-outline mb-4'>
+    //       <input
+    //           type='text'
+    //           placeholder='Your name'
+    //           value={input}
+    //           name='text'
+    //           className='contact-input'
+    //           onChange={handleChange}
+    //         ></input>
+    //       </div>
+    //       <div className='form-outline mb-4'>
+    //         <input
+    //           type='email'
+    //           placeholder='Email address'
+    //           value={input}
+    //           name='text'
+    //           className='contact-input'
+    //           onChange={handleChange}
+    //         ></input>
+    //       </div>
+    //       <div className='form-outline mb-4'>
+    //         <textarea
+    //           rows='4'
+    //           type='text'
+    //           placeholder='Message'
+    //           value={input}
+    //           name='text'
+    //           className='contact-input'
+    //           onChange={handleChange}
+    //         ></textarea>
+    //       </div>
+    //       <button className='contact-button'>Send</button>
+    //     </form>
+    //   </div>
+    // </div>
