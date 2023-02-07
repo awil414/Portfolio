996026bcb391f2cf06a8c40aa444f969fc1c2103
Amber Wilson <amberwilson@MacBook-Pro.local>
2023-02-06 20:56:36 -0600

    adding icons to nav links, border to about me container

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,15 +9,15 @@ const styles = {
   headerStyle: {
     background: "#0096FF",
   },
-  headingStyle: {
-    fontSize: "40px",
-    textAlign: "center",
-    verticalAlign: "middle",
-    lineHeight: "90px",
-    color: "white",
-    padding: "10px",
-    marginBottom: "0",
-  },
+  // headingStyle: {
+  //   fontSize: "40px",
+  //   textAlign: "center",
+  //   verticalAlign: "middle",
+  //   lineHeight: "90px",
+  //   color: "white",
+  //   padding: "10px",
+  //   marginBottom: "0",
+  // },
   skillsStyle: {
     fontSize: "20px",
     textAlign: "center",
@@ -30,15 +30,15 @@ const styles = {
 function Header({ setCurrentPage }) {
   return (
     <div style={styles.navHeader} className='header-nav'>
-      {/* <div style={styles.headerStyle} className='header'>
-        <span>
+      <div style={styles.headerStyle} className='header'>
+        {/* <span>
           <h2 style={styles.headingStyle}>portfolio of Amber Wilson</h2>
           <p style={styles.skillsStyle}>
             Full-Stack Developer | JavaScript | MongoDB | Express | React | Node
             |
           </p>
-        </span>
-      </div> */}
+        </span> */}
+      </div>
       <Navbar setCurrentPage={setCurrentPage} />
     </div>
   );
